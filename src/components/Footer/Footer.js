593d46f8b4d3eed590b614ab09690ac7bef44c2a
Hari20-02 <sriharishankar@outlook.sg>
2023-05-27 18:43:21 -0400

    Initial

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,21 @@
+const Footer = () => {
+    return (
+      <div
+        style={{
+          textAlign: "center",
+          marginBottom: 10,
+        }}
+      >
+        Made by Sri Hari Shankar
+        {/* <a
+          href="https://www.youtube.com/roadsidecoder"
+          style={{ cursor: "pointer" }}
+        >
+          Roadside Coder
+        </a> */}
+      </div>
+    );
+  };
+  
+  export default Footer;
+  
